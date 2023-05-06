fb0ca4c74a320895ae829f85fe6ca46ae8b910e3
KareenaNS <129807816+KareenaNS@users.noreply.github.com>
2023-05-06 16:28:08 -0400

    Add files via upload

---
+++ 1055726.h
@@ -0,0 +1,32 @@
+
+//{{BLOCK(_055726)
+
+//======================================================================
+//
+//	_055726, 256x256@4, 
+//	+ palette 256 entries, not compressed
+//	+ 414 tiles (t|f|p reduced) not compressed
+//	+ regular map (in SBBs), not compressed, 32x32 
+//	Total size: 512 + 13248 + 2048 = 15808
+//
+//	Time-stamp: 2023-04-25, 15:22:51
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT__055726_H
+#define GRIT__055726_H
+
+#define _055726TilesLen 13248
+extern const unsigned short _055726Tiles[6624];
+
+#define _055726MapLen 2048
+extern const unsigned short _055726Map[1024];
+
+#define _055726PalLen 512
+extern const unsigned short _055726Pal[256];
+
+#endif // GRIT__055726_H
+
+//}}BLOCK(_055726)
